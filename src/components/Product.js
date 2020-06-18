ebb999e72b5fb9b060f34a995c7008ffb728fd5f
Lukasz Gaszkowski <lgaszkowski@gmail.com>
2020-06-18 19:45:32 +0200

    init commit

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Product = (props) => {
+  return (
+    <article className="product">
+      {props.id}
+    </article>
+  );
+}
+
+export default Product;
---
+++ src/layouts/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './Header';
+import Navigation from './Navigation';
+import Page from './Page';
+import Footer from './Footer';
+import "../styles/App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className='app'>
+          <header>
+            {<Header />}
+          </header>
+          <main>
+            <aside>
+              {<Navigation />}
+            </aside>
+            <section className='blog'>
+              {<Page />}
+            </section>
+          </main>
+          <footer>
+            {<Footer />}
+          </footer>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
