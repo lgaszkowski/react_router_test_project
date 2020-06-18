671f8e7abb0c1f3ec4a53287808cf194ecf371ab
Lukasz Gaszkowski <lgaszkowski@gmail.com>
2020-06-18 21:24:09 +0200

    Updates

---
+++ precache-manifest.7cf44ba18f13fdcbea3ce4c0a19b0297.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fc2081b7b65b62d4e32",
+    "url": "/react_router_test_project/static/css/main.e1162f81.chunk.css"
+  },
+  {
+    "revision": "8fc2081b7b65b62d4e32",
+    "url": "/react_router_test_project/static/js/main.8fc2081b.chunk.js"
+  },
+  {
+    "revision": "4235d7cfd4c96fa61b17",
+    "url": "/react_router_test_project/static/js/runtime~main.4235d7cf.js"
+  },
+  {
+    "revision": "1226b14d77066b3c2121",
+    "url": "/react_router_test_project/static/js/2.1226b14d.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react_router_test_project/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react_router_test_project/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react_router_test_project/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "2eb3c4d2c4ff246c40ee1fe47a31345e",
+    "url": "/react_router_test_project/index.html"
+  }
+];
